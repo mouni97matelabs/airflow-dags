b8c1252d760cfbfd6dec272c0f1febbe183ae0c8
mouni97matelabs <42138698+mouni97matelabs@users.noreply.github.com>
2020-09-17 12:40:06 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 from airflow import DAG
+import time
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 
 def print_hello():
+ time.sleep(10)
  return 'Hello Wolrd'
 
 dag = DAG('hello_world', description='Hello world example', schedule_interval='0 12 * * *', start_date=datetime(2020, 9, 16), catchup=False)
